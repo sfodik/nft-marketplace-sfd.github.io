30af17d5e4720fc369ef17c7ad42eb3d6026936b
Alex <sfodiksfodin@gmail.com>
2022-05-17 21:29:54 +0300

    Added apoi

--- src/store/actions/actions.ts
+++ src/store/actions/actions.ts
@@ -4,7 +4,11 @@ export const getAssets = (payload: any) => ({type: GET_MARKET, payload})
 
 export const getMarket = () => {
   return (dispatch: any) => {
-    fetch(`https://testnets-api.opensea.io/assets?asset_contract_address=0x646B56b7775e357DA5c35Cd2f8441Bb6aC0EDce1&format=json&order_direction=asc`)
+    fetch(`https://testnets-api.opensea.io/assets?asset_contract_address=0x5A027A1F234a9499a8eAf308a9d318794633d2A8&order_direction=asc`, {
+        method: "GET",
+        mode: "no-cors"
+      }, 
+    ) 
       .then((response) => response.json())
       .then((result) =>
         dispatch(getAssets(result))
